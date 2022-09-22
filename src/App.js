7961ee2800ed57d1b9a5b019509b763ce5ef22a3
Vicka <vickatechlab@gmail.com>
2022-09-22 01:12:57 -0300

    PreEntrega 1 Gonzalez Amato

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import CartWidget from './components/CartWidget';
+import ItemList from './components/ItemList';
+import ItemListContainer from './components/ItemListContainer';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <h1>¡Hola Mundo!</h1>
+    <>
+      <NavBar></NavBar>
+      <ItemListContainer greeting={'¡Te damos la bienvenida!'}></ItemListContainer>
+      <ItemList></ItemList>
+      <CartWidget></CartWidget>
+    </>
   );
 }
 
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,6 @@
+const ItemListContainer = ({greeting}) => {
+  return (
+    <div className="m-5">{greeting}</div>
+  )
+}
+export default ItemListContainer
